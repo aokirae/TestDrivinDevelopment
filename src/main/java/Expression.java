69bf9fc9549e4f74dfdf2fa8775b0511575e00e5
nacl <kamima.yuuya@gmail.com>
2020-09-06 20:08:23 +0900

    poi

--- src/main/java/Expression.java
+++ src/main/java/Expression.java
@@ -1,5 +1,7 @@
 public interface Expression {
 	Expression plus(Expression addend);
 
+	Expression times(int multiplier);
+
 	Money reduce(Bank bank, String to);
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -74,12 +74,47 @@ public class MoneyTest {
 	@Test
 	public void testMixedAddition() {
 		Expression fiveBucks = Money.dollar(5);
-		Expression tenBucks = Money.franc(10);
+		Expression tenFrancs = Money.franc(10);
 
 		Bank bank = new Bank();
 		bank.addRate("CHF", "USD", 2);
 
-		Money result = bank.reduce(fiveBucks.plus(tenBucks), "USD");
+		Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
 		assertEquals(Money.dollar(10), result);
 	}
+
+	@Test
+	public void testSumPlusMoney() {
+		Expression fiveBucks = Money.dollar(5);
+		Expression tenFrancs = Money.franc(10);
+
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+
+		Expression sum = new Sum(fiveBucks, tenFrancs);
+		Money result = bank.reduce(sum, "USD");
+
+		// 本の誤植
+		assertEquals(Money.dollar(10), result);
+	}
+
+	@Test
+	public void testSumTimes() {
+		Expression fiveBucks = Money.dollar(5);
+		Expression tenFrancs = Money.franc(10);
+
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+
+		Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+		Money result = bank.reduce(sum, "USD");
+
+		assertEquals(Money.dollar(20), result);
+	}
+
+	// @Test
+	// public void testPlusSameCurrencyReturnsMoney() {
+	// Expression sum = Money.dollar(1).plus(Money.dollar(1));
+	// assertTrue(sum instanceof Money);
+	// }
 }

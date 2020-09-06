8dada63f4557ef87cd2b76fc24e136c0968d47fd
nacl <kamima.yuuya@gmail.com>
2020-09-06 13:24:56 +0900

    poi

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -15,8 +15,12 @@ class Money implements Expression {
 		return new Money(amount * multiplier, currency);
 	}
 
-	Expression plus(Money added) {
-		return new Money(amount + added.amount, currency);
+	Expression plus(Money addend) {
+		return new Sum(this, addend);
+	}
+
+	public Money reduce(String to) {
+		return this;
 	}
 
 	public boolean equals(Object object) {
